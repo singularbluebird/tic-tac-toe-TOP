aa1645bf6eec1a89b6a8a4d603df15bed7b4c9a0
Samuel <samysarria.ss@gmail.com>
2025-04-02 21:22:05 -0300

    Populating the Player.rb file

--- Player.rb
+++ Player.rb
@@ -1,3 +1,17 @@
-def class Player
+class Player
+  attr_accessor :name, :next_move, :player_symbol
+  # This variable of number of players should not be higher than one. Which is why I will make it only accessible to the Game class so that nobody can increase its count beyond the desired limit.
+  @@number_of_players = 0
+
+  def initialize(name)
+    @name = name
+    @next_move = nil
+    @player_symbol = @@number_of_players == 0 ? 'x' : 'o'
+    @@number_of_players += 1
+  end
+
+  def make_move()
+    @next_move = gets.chomp.to_i
+  end
 
 end
